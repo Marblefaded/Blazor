c7761c2c78354bf6a8c940fa3ee4d4e45191eaf5
Marblefade <sitnikov.vladimiir@mail.ru>
2023-04-29 00:00:54 +0700

    Activated input button

--- BlazorApp/Pages/Resume.razor.cs
+++ BlazorApp/Pages/Resume.razor.cs
@@ -4,6 +4,20 @@ namespace BlazorApp.Pages
 {
     public class ResumeViewModel:ComponentBase
     {
-        
+        public string currentHeading = "";
+        public string? newHeading;
+        public bool value;
+
+        public void pushInfo()
+        {
+            if (!value)
+            {
+                value = true;
+            }
+            else
+            {
+                value= false;
+            }
+        }
     }
 }
