b89bd7538e1cc374408ffbaffb8d710e7602e94f
Marblefade <sitnikov.vladimiir@mail.ru>
2023-04-29 15:57:43 +0700

    Edit is working and add some functions

--- BlazorApp/Pages/Resume.razor.cs
+++ BlazorApp/Pages/Resume.razor.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Data;
+using BlazorApp.Data;
 
 namespace BlazorApp.Pages
 {
-    public class ResumeViewModel:ComponentBase
+    public class ResumeViewModel : ComponentBase
     {
         public string currentHeading = "";
-        public string? newHeading;
-        public bool value;
+        public DataPage data = new DataPage();
+
+        public bool value { get; set; }
+        protected override void OnInitialized()
+        {
+            pushInfo();
+        }
 
         public void pushInfo()
+        {
+            
+
+            if (!value)
+            {
+                value = true;
+            }
+            else
+            {
+                value = false;
+            }
+            using (FileStream fs = new FileStream("DataPage.json", FileMode.OpenOrCreate))
+            {
+                data = JsonSerializer.Deserialize<DataPage>(fs);
+                StateHasChanged();
+            }
+
+        }
+
+
+        public void saveInfo(DataPage data)
         {
             if (!value)
             {
@@ -16,8 +47,26 @@ namespace BlazorApp.Pages
             }
             else
             {
-                value= false;
+                value = false;
             }
+            using (FileStream fs = new FileStream("DataPage.json", FileMode.OpenOrCreate))
+            {
+                string jsonString = JsonSerializer.Serialize<DataPage>(data);
+                byte[] array = Encoding.Default.GetBytes(jsonString);
+                fs.WriteAsync(array, 0, array.Length);
+                StateHasChanged();
+            }
+            
         }
+
+        public void cancelInfo()
+        {
+            value = false;
+            
+        }
+
+
+
     }
+
 }
