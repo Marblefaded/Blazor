bc0aa280e1c9e2de36e05a0ab13900e00983b589
Marblefade <sitnikov.vladimiir@mail.ru>
2023-05-02 23:31:38 +0700

    Add stopwatch and timer

---
+++ BlazorApp/Data/DataPage.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Data
+{
+    public class DataPage
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Gender { get; set; }
+        public string? Number { get; set; }
+        public string? Metro{ get; set; }
+
+    }
+}
